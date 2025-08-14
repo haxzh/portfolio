5c4e246b5401713206dd39ba2daacb9c92a2574d
Harshakya <168418718+haxzh@users.noreply.github.com>
2025-08-14 21:31:22 +0530

    Fix unused variable 'year' in Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,82 @@
+// import React from "react";
+// import { Container, Row, Col } from "react-bootstrap";
+// import {
+//   AiFillGithub,
+//   AiOutlineTwitter,
+//   AiFillInstagram,
+// } from "react-icons/ai";
+// import { FaLinkedinIn } from "react-icons/fa";
+
+// function Footer() {
+//   let date = new Date();
+//   let year = date.getFullYear();
+//   return (
+//     <Container fluid className="footer">
+//       <Row>
+//         <Col md="4" className="footer-copywright">
+//           <h3>Designed and Developed by Harsh Kumar</h3>
+//         </Col>
+//         {/* <Col md="4" className="footer-copywright">
+//           <h3>Copyright © {year} SB</h3>
+//         </Col> */}
+//         <Col md="4" className="footer-body">
+//           <ul className="footer-icons">
+//             <li className="social-icons">
+//               <a
+//                 href="https://github.com/haxzh"
+//                 style={{ color: "white" }}
+//                 target="_blank" 
+//                 rel="noopener noreferrer"
+//               >
+//                 <AiFillGithub />
+//               </a>
+//             </li>
+//             <li className="social-icons">
+//               <a
+//                 href="https://twitter.com/harsh_shakya_84"
+//                 style={{ color: "white" }}
+//                 target="_blank" 
+//                 rel="noopener noreferrer"
+//               >
+//                 <AiOutlineTwitter />
+//               </a>
+//             </li>
+//             <li className="social-icons">
+//               <a
+//                 href="https://www.linkedin.com/in/haxzh"
+//                 style={{ color: "white" }}
+//                 target="_blank" 
+//                 rel="noopener noreferrer"
+//               >
+//                 <FaLinkedinIn />
+//               </a>
+//             </li>
+//             <li className="social-icons">
+//               <a
+//                 href="https://www.instagram.com/am.haxzh"
+//                 style={{ color: "white" }}
+//                 target="_blank" 
+//                 rel="noopener noreferrer"
+//               >
+//                 <AiFillInstagram />
+//               </a>
+//             </li>
+//           </ul>
+//         </Col>
+//       </Row>
+//     </Container>
+//   );
+// }
+
+// export default Footer;
+
+
+
+
+
+
+
+
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import {
@@ -8,24 +87,25 @@ import {
 import { FaLinkedinIn } from "react-icons/fa";
 
 function Footer() {
-  let date = new Date();
-  let year = date.getFullYear();
+  const date = new Date();
+  const year = date.getFullYear();
+
   return (
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
           <h3>Designed and Developed by Harsh Kumar</h3>
         </Col>
-        {/* <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} SB</h3>
-        </Col> */}
+        <Col md="4" className="footer-copywright">
+          <h3>Copyright © {year} Harsh Kumar</h3>
+        </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
             <li className="social-icons">
               <a
                 href="https://github.com/haxzh"
                 style={{ color: "white" }}
-                target="_blank" 
+                target="_blank"
                 rel="noopener noreferrer"
               >
                 <AiFillGithub />
@@ -35,7 +115,7 @@ function Footer() {
               <a
                 href="https://twitter.com/harsh_shakya_84"
                 style={{ color: "white" }}
-                target="_blank" 
+                target="_blank"
                 rel="noopener noreferrer"
               >
                 <AiOutlineTwitter />
@@ -45,7 +125,7 @@ function Footer() {
               <a
                 href="https://www.linkedin.com/in/haxzh"
                 style={{ color: "white" }}
-                target="_blank" 
+                target="_blank"
                 rel="noopener noreferrer"
               >
                 <FaLinkedinIn />
@@ -55,7 +135,7 @@ function Footer() {
               <a
                 href="https://www.instagram.com/am.haxzh"
                 style={{ color: "white" }}
-                target="_blank" 
+                target="_blank"
                 rel="noopener noreferrer"
               >
                 <AiFillInstagram />
@@ -69,3 +149,4 @@ function Footer() {
 }
 
 export default Footer;
+
